7a1338ce958a931862a20a79f7a96a334a2c476d
CG <8009705+coggro@users.noreply.github.com>
2021-07-06 09:41:12 -0400

    Through video 092

--- src/index.js
+++ src/index.js
@@ -20,6 +20,30 @@ app.post(`/users`, (req, res) => {
     })
 })
 
+app.get(`/users`, (req, res) => {
+  User.find({})
+    .then((users) => {
+      res.status(200).send(users)
+    })
+    .catch((e) => {
+      res.status(500).send()
+    })
+})
+
+app.get(`/users/:id`, (req, res) => {
+  const _id = req.params.id
+  User.findById(_id)
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send()
+      }
+      res.status(200).send(user)
+    })
+    .catch((e) => {
+      res.status(500).send(e)
+    })
+})
+
 app.post(`/tasks`, (req, res) => {
   const task = new Task(req.body)
   task
@@ -29,6 +53,30 @@ app.post(`/tasks`, (req, res) => {
     })
     .catch((e) => {
       res.status(400).send(e)
+    })
+})
+
+app.get(`/tasks`, (req, res) => {
+  Task.find({})
+    .then((tasks) => {
+      res.status(200).send(tasks)
+    })
+    .catch((e) => {
+      res.status(500).send()
+    })
+})
+
+app.get(`/tasks/:id`, (req, res) => {
+  const _id = req.params.id
+  Task.findById(_id)
+    .then((task) => {
+      if (!task) {
+        return res.status(404).send()
+      }
+      res.status(200).send(task)
+    })
+    .catch((e) => {
+      res.status(500).send()
     })
 })
 
