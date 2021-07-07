4a890c08c68f1472e39ac54e91d65e66d0eebf43
CG <8009705+coggro@users.noreply.github.com>
2021-07-07 01:42:00 -0400

    Add update and delete, refactor to router files

--- src/index.js
+++ src/index.js
@@ -1,78 +1,15 @@
 import express from 'express'
 
-import User from './models/user.js'
-import Task from './models/task.js'
+import userRouter from '../src/routers/user.js'
+import taskRouter from '../src/routers/task.js'
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
 
-app.post(`/users`, async (req, res) => {
-  const user = new User(req.body)
-  try {
-    await user.save()
-
-    res.status(201).send(user)
-  } catch (e) {
-    res.status(400).send(e)
-  }
-})
-
-app.get(`/users`, async (req, res) => {
-  try {
-    const users = await User.find({})
-    res.status(200).send(users)
-  } catch (e) {
-    res.status(500).send()
-  }
-})
-
-app.get(`/users/:id`, (req, res) => {
-  const _id = req.params.id
-  User.findById(_id)
-    .then((user) => {
-      if (!user) {
-        return res.status(404).send()
-      }
-      res.status(200).send(user)
-    })
-    .catch((e) => {
-      res.status(500).send(e)
-    })
-})
-
-app.post(`/tasks`, async (req, res) => {
-  const task = new Task(req.body)
-  try {
-    await task.save()
-    res.status(201).send(task)
-  } catch (e) {
-    res.status(400).send(e)
-  }
-})
-
-app.get(`/tasks`, async (req, res) => {
-  try {
-    const tasks = await Task.find({})
-    res.status(200).send(tasks)
-  } catch (e) {
-    res.status(500).send()
-  }
-})
-
-app.get(`/tasks/:id`, async (req, res) => {
-  const _id = req.params.id
-  try {
-    const task = await Task.findById(_id)
-    if (!task) {
-      return res.status(404).send()
-    }
-    res.status(200).send(task)
-  } catch (e) {
-    res.status(500).send()
-  }
-})
+app.use(userRouter)
+app.use(taskRouter)
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}.`)
---
+++ src/routers/task.js
@@ -0,0 +1,77 @@
+import express from 'express'
+
+import Task from '../models/task.js'
+
+const router = new express.Router()
+
+router.post(`/tasks`, async (req, res) => {
+  const task = new Task(req.body)
+  try {
+    await task.save()
+    res.status(201).send(task)
+  } catch (e) {
+    res.status(400).send(e)
+  }
+})
+
+router.get(`/tasks`, async (req, res) => {
+  try {
+    const tasks = await Task.find({})
+    res.status(200).send(tasks)
+  } catch (e) {
+    res.status(500).send()
+  }
+})
+
+router.get(`/tasks/:id`, async (req, res) => {
+  const _id = req.params.id
+  try {
+    const task = await Task.findById(_id)
+    if (!task) {
+      return res.status(404).send()
+    }
+    res.status(200).send(task)
+  } catch (e) {
+    res.status(500).send()
+  }
+})
+
+router.patch(`/tasks/:id`, async (req, res) => {
+  const _id = req.params.id
+  const updates = Object.keys(req.body)
+  const allowedUpdates = [`description`, `completed`]
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  )
+  if (!isValidOperation) {
+    return res.status(400).send({ error: `Invalid updates!` })
+  }
+  try {
+    const task = await Task.findByIdAndUpdate(_id, req.body, {
+      new: true,
+      runValidators: true,
+    })
+    if (!task) {
+      return res.status(404).send()
+    }
+    res.status(200).send(task)
+  } catch (e) {
+    res.status(400).send(e)
+  }
+})
+
+router.delete(`/tasks/:id`, async (req, res) => {
+  const _id = req.params.id
+  try {
+    const task = await Task.findByIdAndDelete(_id)
+    console.log(task)
+    if (!task) {
+      return res.status(404).send()
+    }
+    res.status(200).send(task)
+  } catch (e) {
+    res.status(500).send(e)
+  }
+})
+
+export default router
