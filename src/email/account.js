5e6788aa212792a7622cd84daa30d803fc78425e
CG <8009705+coggro@users.noreply.github.com>
2021-07-23 23:04:55 -0400

    Temporary fix by adding env var

--- src/email/account.js
+++ src/email/account.js
@@ -3,23 +3,27 @@ import sendgrid from '@sendgrid/mail'
 sendgrid.setApiKey(process.env.SENDGRID_API_KEY)
 
 const sendWelcomeEmail = (email, name) => {
-  // sendgrid.send({
-  //   to: email,
-  //   from: `corey@coreyhgross.com`,
-  //   subject: `Thanks for joining in!`,
-  //   text: `Welcome to the app, ${name}. Let me know how you get along with the app.`,
-  // })
-  console.log(`Send Welcome Email`)
+  if (process.env.SENDGRID_TEST) {
+    return console.log(`Don't bother.`)
+  }
+  sendgrid.send({
+    to: email,
+    from: `corey@coreyhgross.com`,
+    subject: `Thanks for joining in!`,
+    text: `Welcome to the app, ${name}. Let me know how you get along with the app.`,
+  })
 }
 
 const sendCancellationEmail = (email, name) => {
-  // sendgrid.send({
-  //   to: email,
-  //   from: `corey@coreyhgross.com`,
-  //   subject: `Sorry to see you go!`,
-  //   text: `What did we do that hurt you so? Let us know how we can do better!`,
-  // })
-  console.log(`Send Cancellation Email`)
+  if (process.env.SENDGRID_TEST) {
+    return console.log(`Don't bother.`)
+  }
+  sendgrid.send({
+    to: email,
+    from: `corey@coreyhgross.com`,
+    subject: `Sorry to see you go!`,
+    text: `What did we do that hurt you so? Let us know how we can do better!`,
+  })
 }
 
 export { sendWelcomeEmail, sendCancellationEmail }
